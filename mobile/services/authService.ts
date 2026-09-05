21af52aa4613c56a6c4fe8bfb7bb444a14dda927
Abubakar Abdullahi <superango93@gmail.com>
2026-09-05 06:56:00 +0100

    fix

--- mobile/services/authService.ts
+++ mobile/services/authService.ts
@@ -5,10 +5,29 @@ export interface LoginData {
   password: string;
 }
 
+
+export interface RegisterData {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+}
+
 export const loginUser = async (
   data: LoginData
 ) => {
   return apiRequest("/auth/login", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+};
+
+
+
+export const registerUser = async (
+  data: RegisterData
+) => {
+  return apiRequest("/auth/register", {
     method: "POST",
     body: JSON.stringify(data),
   });
